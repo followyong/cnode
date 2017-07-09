f41f8a861394fe0eef75ead192201ec7f13e3478
followyong <947690826@qq.com>
2017-07-09 16:32:54 +0800

    gh-pages branch

--- Desktop/cnode-demo/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import 'antd/dist/antd.css';
-import './main.css'
-
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
